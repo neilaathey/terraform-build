6e73156601901e10ce0bb5834a3a0ab47e8d1509
NeilAAthey <neilaathey@gmail.com>
2021-04-23 10:38:50 +0100

    testing Dataset Transfer v7

--- infra/main.tf
+++ infra/main.tf
@@ -8,16 +8,42 @@ variable "project_id" {
   description = "Project ID to deploy resources in."
 }
 
+resource "google_bigquery_dataset" "useful_backup" {
+  #depends_on = [google_project_iam_member.permissions]
+  project       = var.project_id
+  dataset_id    = "useful_backup"
+  friendly_name = "foo"
+  description   = "bar"
+  location      = "EU"
+}
+
+# resource "google_bigquery_data_transfer_config" "test_copy_dataset_config" {
+#   project                = var.project_id
+#   display_name           = "test_copy_dataset_config_display_name"
+#   location               = "EU"
+#   schedule               = "first sunday of quarter 00:00"
+#   data_source_id         = "scheduled_query"
+#   destination_dataset_id = google_bigquery_dataset.useful_backup.dataset_id
+#   service_account_name   = "test-terraform"
+#   params = {
+#     source_dataset_id           = "useful"
+#     source_project_id           = var.project_id
+#     write_disposition           = "WRITE_TRUNCATE"
+
+#   }
+# }
+
 resource "google_bigquery_data_transfer_config" "test_copy_dataset_config" {
   project                = var.project_id
-  display_name           = "test_copy_dataset_config"
+  display_name           = "test_copy_dataset_config_display_name"
   location               = "EU"
   schedule               = "every mon, tue, wed, thu, fri 02:00"
   data_source_id         = "useful"
-  destination_dataset_id = "useful_backup"
+  destination_dataset_id = google_bigquery_dataset.useful_backup.dataset_id
   params = {
+    overwrite_destination_table = "true"
     source_dataset_id           = "useful"
-    source_project_id           = var.project_id
-    overwrite_destination_table = true
   }
 }
+
+#terraform import google_bigquery_data_transfer_config.default test_copy_dataset_config

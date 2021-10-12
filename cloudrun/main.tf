f9165e6f4a5b4ed7d8c16350705095ed17f03b89
NeilAAthey <neilaathey@gmail.com>
2021-10-12 11:27:36 +0100

    switched projects to dev

---
+++ cloudrun/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "gcs" {
+    prefix = "cloudrun"
+    bucket = "cubicbi-dev-tf-state"
+  }
+}
+
+variable "project_id" {
+  type        = string
+  description = "Project ID to deploy resources in."
+  default = "cubicbi-dev"
+}
+
+# output "test_output" {
+#   value = "Hello Neil!"  
+# }
+
+resource "google_cloud_run_service" "default" {
+  name     = "gcp-cloud-run-tf-v2-using-cloudbuild-image"
+  location = "europe-west2"
+  project = var.project_id
+
+  template {
+    spec {
+      timeout_seconds = 600
+      containers {
+        image = "gcr.io/cubicbi-cloudrun/cloudrun"  
+        //"gcr.io/cubicbi-cloudrun/gcp-cloud-run-v-2"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
